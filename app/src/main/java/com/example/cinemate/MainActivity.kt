ec47b5520c61cc3271cf5916af8398f2132851bb
YuneeeM <dana512@naver.com>
2023-12-12 22:23:32 +0900

    fix #5 : 바텀 네비 수정

--- app/src/main/java/com/example/cinemate/MainActivity.kt
+++ app/src/main/java/com/example/cinemate/MainActivity.kt
@@ -1,22 +1,54 @@
 package com.example.cinemate
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.cinemate.ui.theme.CinemateTheme
+import com.example.cinemate.databinding.ActivityMainBinding
+import com.example.cinemate.fragment.HomeFragment
+import com.example.cinemate.fragment.MypageFragment
+import com.example.cinemate.fragment.PeopleFragment
+import com.example.cinemate.fragment.SearchFragment
 
 class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        // ViewBinding
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.activityMainBottomMenu.run {
+            setOnItemSelectedListener {
+                when(it.itemId){
+                    R.id.menu_home -> {
+                        val homeFragment = HomeFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.activity_main_fl,homeFragment).commit()
+                    }
+                    R.id.menu_together -> {
+                        val peopleFragment = PeopleFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.activity_main_fl,peopleFragment).commit()
+                    }
+                    R.id.menu_search -> {
+                        val searchFragment = SearchFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.activity_main_fl,searchFragment).commit()
+                    }
+                    R.id.menu_my_page -> {
+                        val mypageFragment = MypageFragment()
+                        supportFragmentManager.beginTransaction().replace(R.id.activity_main_fl,mypageFragment).commit()
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.menu_home
+        }
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
 
